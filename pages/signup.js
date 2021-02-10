b192b3fce25a3e13ed406a48f5ae002e0a359295
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-10 15:21:12 +0900

    :construction:Green: signup.js input 컴포넌트 생성

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,16 @@
+import { Input } from 'antd';
+
 function SignUp() {
-  return <div>회원가입</div>;
+  return (
+    <div className="signup-form-container">
+      <Input placeholder="아이디" />
+      <Input placeholder="비밀번호" />
+      <Input placeholder="비밀번호 확인" />
+      <Input placeholder="이름" />
+      <Input placeholder="이메일" />
+      <Input placeholder="연락처" />
+    </div>
+  );
 }
 
 export default SignUp;
