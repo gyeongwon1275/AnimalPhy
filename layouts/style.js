7b891d68985d956c55fcfeb8c206f0f0c91fad8d
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-14 12:16:04 +0900

    :lipstick: Style: 회원가입 style 추가

---
+++ layouts/style.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.div`
+  width: 300px;
+  height: auto;
+  margin: 0 auto;
+
+  .logo {
+    padding: 30px;
+    text-align: center;
+  }
+
+  .form-input-wrapper {
+    padding: 8px;
+    margin: 8px;
+  }
+  .form-button-wrapper {
+    margin: 8px;
+    width: 100%;
+  }
+  .go-to-login-wrapper {
+    margin: 8px;
+    text-align: center;
+    p {
+      display: inline;
+    }
+    a {
+      color: #0092ff;
+      font-weight: 900;
+    }
+  }
+`;
