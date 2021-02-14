a0a55fc40595d431e93aaacec90d307d78dfa985
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-14 12:14:31 +0900

    :construction: Working-Green: signup 에 input 추가
    - test 통과하도록 input 추가

---
+++ layouts/PublicLayout.js
@@ -0,0 +1,12 @@
+import { FormContainer } from './style';
+
+function PublicLayout({ children }) {
+  return (
+    <FormContainer className="form-container">
+      <h1 className="logo">{'🐶 AnimalPhy'}</h1>
+      {children}
+    </FormContainer>
+  );
+}
+
+export default PublicLayout;
--- pages/signup.js
+++ pages/signup.js
@@ -1,15 +1,97 @@
-import { Input } from 'antd';
+import { Button, Input } from 'antd';
+import { useFormik } from 'formik';
+import PublicLayout from '@layouts/PublicLayout';
 
 function SignUp() {
+  const formik = useFormik({
+    initialValues: {
+      userId: '',
+      password: '',
+      passwordConfirm: '',
+      userName: '',
+      email: '',
+      phone: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <div className="signup-form-container">
-      <Input placeholder="아이디" />
-      <Input placeholder="비밀번호" />
-      <Input placeholder="비밀번호 확인" />
-      <Input placeholder="이름" />
-      <Input placeholder="이메일" />
-      <Input placeholder="연락처" />
-    </div>
+    <PublicLayout>
+      <form className="signup-form" onSubmit={formik.handleSubmit}>
+        <input type="submit" style={{ display: 'none' }} />
+        <Input
+          className="form-input-wrapper"
+          placeholder="아이디"
+          id="userId"
+          name="userId"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.userId}
+          required
+        />
+        <Input.Password
+          className="form-input-wrapper"
+          placeholder="비밀번호"
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          required
+        />
+        <Input.Password
+          className="form-input-wrapper"
+          placeholder="비밀번호 확인"
+          id="passwordConfirm"
+          name="passwordConfirm"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.passwordConfirm}
+          required
+        />
+        <Input
+          className="form-input-wrapper"
+          placeholder="이름"
+          id="userName"
+          name="userName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.userName}
+        />
+        <Input
+          className="form-input-wrapper"
+          placeholder="이메일"
+          id="email"
+          name="email"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <Input
+          className="form-input-wrapper"
+          placeholder="연락처"
+          id="phone"
+          name="phone"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.phone}
+        />
+        <Button
+          className="form-button-wrapper"
+          type="submit"
+          onClick={formik.handleSubmit}
+        >
+          회원가입
+        </Button>
+      </form>
+
+      <div className="go-to-login-wrapper">
+        <p>이미 회원이세요? </p>
+        <a href="/login">로그인</a>
+      </div>
+    </PublicLayout>
   );
 }
 
