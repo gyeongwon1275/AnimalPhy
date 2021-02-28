928388b9b8ae0e4b8ab982d5f97d74f705ac4f72
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-28 16:20:50 +0900

    :construction: Green-Working: rootReducer
    - index

--- data/rootReducer.js
+++ data/rootReducer.js
@@ -1,19 +1,13 @@
 import { HYDRATE } from 'next-redux-wrapper';
 import { combineReducers } from 'redux';
 
-import user from './user/reducer';
+export function index(state = {}, { type, payload }) {
+  return state;
+}
 
 const rootReducer = combineReducers({
-  index: (state = {}, action) => {
-    switch (action.type) {
-      case HYDRATE:
-        console.log('HYDRATE', action);
-        return { ...state, ...action.payload };
-      default:
-        return state;
-    }
-  },
-  user,
+  index,
+
 });
 
 export default rootReducer;
