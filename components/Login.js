9771c2a30d6356313eb53106396d3bc4aa66c844
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-28 16:52:34 +0900

    :construction: Green-Working:Login
    - 회원가입으로 이동하는 링크

--- components/Login.js
+++ components/Login.js
@@ -3,27 +3,33 @@ import { Input, Form } from 'antd';
 
 export default function Login({ formik: { values, handleChange } }) {
   return (
-    <Form className="login-form">
-      <Input
-        className="form-input-wrapper"
-        placeholder="아이디"
-        id="userId"
-        name="userId"
-        type="text"
-        onChange={handleChange}
-        value={values.userId}
-        required
-      />
-      <Input.Password
-        className="form-input-wrapper"
-        placeholder="비밀번호"
-        id="password"
-        name="password"
-        type="password"
-        onChange={handleChange}
-        value={values.password}
-        required
-      />
-    </Form>
+    <>
+      <Form className="login-form">
+        <Input
+          className="form-input-wrapper"
+          placeholder="아이디"
+          id="userId"
+          name="userId"
+          type="text"
+          onChange={handleChange}
+          value={values.userId}
+          required
+        />
+        <Input.Password
+          className="form-input-wrapper"
+          placeholder="비밀번호"
+          id="password"
+          name="password"
+          type="password"
+          onChange={handleChange}
+          value={values.password}
+          required
+        />
+      </Form>
+      <div className="go-to-signup-wrapper">
+        <p>계정이 없나요? </p>
+        <a href="/signup">회원가입</a>
+      </div>
+    </>
   );
 }
