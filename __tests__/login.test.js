0080127dabe67c47cec59b1cb315bd224820c2d7
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-28 16:32:18 +0900

    :white_check_mark: Red-Test: login
    - 값 입력 가능한지

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { useFormik } from 'formik';
 
 import LoginContainer from '@pages/login';
 
 describe('Login', () => {
+  const handleChange = jest.fn();
   beforeEach(() => {
     jest.clearAllMocks();
     useFormik.mockImplementationOnce(() => ({
@@ -12,6 +13,7 @@ describe('Login', () => {
         userId: '',
         password: '',
       },
+      handleChange,
     }));
   });
   it('renders id,password,name,email,phone form', () => {
@@ -19,5 +21,19 @@ describe('Login', () => {
 
     expect(getByPlaceholderText('아이디')).toBeInTheDocument();
     expect(getByPlaceholderText('비밀번호')).toBeInTheDocument();
+  });
+
+  it('input id and password', () => {
+    const { getByPlaceholderText } = render(<LoginContainer />);
+
+    fireEvent.change(getByPlaceholderText('아이디'), {
+      target: { value: 'rud285' },
+    });
+    expect(handleChange).toHaveBeenCalledTimes(1);
+
+    fireEvent.change(getByPlaceholderText('비밀번호'), {
+      target: { value: '1234' },
+    });
+    expect(handleChange).toHaveBeenCalledTimes(2);
   });
 });
