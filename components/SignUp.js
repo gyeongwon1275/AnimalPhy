806aa26d0ba08371058e61620a0f272de5029bdb
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-28 16:46:19 +0900

    :construction: Green-Working: signup
    - 로그인으로 이동하는 링크

--- components/SignUp.js
+++ components/SignUp.js
@@ -72,6 +72,10 @@ export default function SignUp({ formik: { values, handleSubmit, handleChange } 
         </Button>
       </Form>
 
+      <div className="go-to-login-wrapper">
+        <p>이미 회원이세요? </p>
+        <a href="/login">로그인</a>
+      </div>
     </>
   );
 }
