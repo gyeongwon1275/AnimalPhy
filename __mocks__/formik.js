44d19b0f0dfbd4a531d36c0a81d10d5c5840bc29
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-28 14:22:19 +0900

    :white_check_mark: Red-Test: sign-up
    - input 출력하는지
    - 값을 입력할 수 있는지
    - 입력한 값을 제출할 수 있는지

---
+++ __mocks__/formik.js
@@ -0,0 +1,1 @@
+export const useFormik =jest.fn();
--- __tests__/signup.test.js
+++ __tests__/signup.test.js
@@ -3,8 +3,31 @@ import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 
 import SignUpContainer from '@pages/signup';
+import { useFormik } from 'formik';
+
+
 
 describe('SignUp', () => {
+
+  const handleSubmit = jest.fn();
+  const handleChange = jest.fn();
+
+  beforeEach(()=>{
+
+    jest.clearAllMocks();
+    useFormik.mockImplementationOnce(()=>({
+      values:{
+        userId: '',
+        password: '',
+        passwordConfirm: '',
+        userName: '',
+        email: '',
+        phone: '',
+      },
+      handleChange,
+      handleSubmit
+    }))
+  })
   it('renders id,password,name,email,phone form', () => {
     const { getByPlaceholderText } = render(<SignUpContainer />);
 
@@ -17,36 +40,52 @@ describe('SignUp', () => {
   });
 
   it('input id,password,name,email,phone form', () => {
+
     const { getByPlaceholderText } = render(<SignUpContainer />);
 
     fireEvent.change(getByPlaceholderText('아이디'), {
       target: { value: 'rud285' },
     });
-    expect(getByPlaceholderText('아이디').value).toBe('rud285');
+    expect(handleChange).toHaveBeenCalledTimes(1);
 
     fireEvent.change(getByPlaceholderText('비밀번호'), {
       target: { value: '1234' },
     });
-    expect(getByPlaceholderText('비밀번호').value).toBe('1234');
+    expect(handleChange).toHaveBeenCalledTimes(2);
+
 
     fireEvent.change(getByPlaceholderText('비밀번호 확인'), {
       target: { value: '1234' },
     });
-    expect(getByPlaceholderText('비밀번호 확인').value).toBe('1234');
+    expect(handleChange).toHaveBeenCalledTimes(3);
+
 
     fireEvent.change(getByPlaceholderText('이름'), {
       target: { value: '김경원' },
     });
-    expect(getByPlaceholderText('이름').value).toBe('김경원');
+    expect(handleChange).toHaveBeenCalledTimes(4);
+
 
     fireEvent.change(getByPlaceholderText('이메일'), {
       target: { value: 'rud285@naver.com' },
     });
-    expect(getByPlaceholderText('이메일').value).toBe('rud285@naver.com');
+    expect(handleChange).toHaveBeenCalledTimes(5);
+
 
     fireEvent.change(getByPlaceholderText('연락처'), {
       target: { value: '010-8690-1275' },
     });
-    expect(getByPlaceholderText('연락처').value).toBe('010-8690-1275');
+    expect(handleChange).toHaveBeenCalledTimes(6);
+
+  });
+
+  it('submit user info', () => {
+ 
+    const { getByText } = render(<SignUpContainer />);
+
+    fireEvent.click(getByText('회원가입'))
+
+    expect(handleSubmit).toHaveBeenCalled();
+
   });
 });
